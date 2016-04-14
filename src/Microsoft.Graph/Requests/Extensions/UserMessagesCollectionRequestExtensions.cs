f73d4070b90096918dea0d2314b94f6345520d3d
James Sturtevant <jsturtevant@gmail.com>
2016-04-14 09:20:00 -0400

    added seach functionality to User Messages (email)

---
+++ src/Microsoft.Graph/Requests/Extensions/UserMessagesCollectionRequestExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Graph
+{
+    public partial class UserMessagesCollectionRequest
+    {
+        public IUserMessagesCollectionRequest Search(string value)
+        {
+            this.QueryOptions.Add(new QueryOption("$search", value));
+            return this;
+        }
+    }
+
+    public partial interface IUserMessagesCollectionRequest
+    {
+        IUserMessagesCollectionRequest Search(string value);
+    }
+}
