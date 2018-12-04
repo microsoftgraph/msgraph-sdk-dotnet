6a8896a30371e876efe384cb47d5939aed4d7226
Client Tooling Big Giant Robot <graphtooling@microsoft.com>
2018-12-03 23:17:13 -0800

    Update generated files with build 827640

--- src/Microsoft.Graph/Requests/Generated/GraphServiceClient.cs
+++ src/Microsoft.Graph/Requests/Generated/GraphServiceClient.cs
@@ -272,6 +272,17 @@ namespace Microsoft.Graph
         }
     
         /// <summary>
+        /// Gets the GraphServiceTeams request builder.
+        /// </summary>
+        public IGraphServiceTeamsCollectionRequestBuilder Teams
+        {
+            get
+            {
+                return new GraphServiceTeamsCollectionRequestBuilder(this.BaseUrl + "/teams", this);
+            }
+        }
+    
+        /// <summary>
         /// Gets the GraphServiceMe request builder.
         /// </summary>
         public IUserRequestBuilder Me
@@ -367,6 +378,17 @@ namespace Microsoft.Graph
             get
             {
                 return new SecurityRequestBuilder(this.BaseUrl + "/Security", this);
+            }
+        }
+    
+        /// <summary>
+        /// Gets the GraphServiceAppCatalogs request builder.
+        /// </summary>
+        public IAppCatalogsRequestBuilder AppCatalogs
+        {
+            get
+            {
+                return new AppCatalogsRequestBuilder(this.BaseUrl + "/appCatalogs", this);
             }
         }
     
