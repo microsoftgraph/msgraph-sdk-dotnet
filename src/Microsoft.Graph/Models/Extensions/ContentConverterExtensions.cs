8f53262c03dbb9d3f638a6a6db5d767f58800e87
Titus Gicheru (Insight Management Consultants) <v-tigich@microsoft.com>
2019-05-23 06:58:09 +0300

    Content Converter Extensions Implementation

---
+++ src/Microsoft.Graph/Models/Extensions/ContentConverterExtensions.cs
@@ -0,0 +1,36 @@
+﻿// ------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------
+
+using System;
+
+namespace Microsoft.Graph.Extensions
+{
+    /// <summary>
+    /// Implements Content Converter Extensions
+    /// </summary>
+    public static class ContentConverterExtensions
+    {
+        /// <summary>
+        /// Converts DateTimeTimeZone which is a Complex Type to DateTime
+        /// </summary>
+        /// <param name="dateTimeTimeZone"></param>
+        /// <returns></returns>
+        public static DateTime ToDateTime(this DateTimeTimeZone dateTimeTimeZone)
+        {
+            DateTime dateTime = DateTime.Parse(dateTimeTimeZone.DateTime);
+            return dateTime;
+        }
+
+        /// <summary>
+        /// Converts DateTimeTimeZone which is a Complex Type to DateTimeOffset
+        /// </summary>
+        /// <param name="dateTimeTimeZone"></param>
+        /// <returns></returns>
+        public static DateTimeOffset ToDateTimeOffset(this DateTimeTimeZone dateTimeTimeZone)
+        {
+            DateTime dateTime = DateTimeOffset.Parse(dateTimeTimeZone.DateTime).UtcDateTime;
+            return dateTime;
+        }
+    }
+}
---
+++ tests/Microsoft.Graph.Test/Models/Extensions/ContentConverterExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Graph.Extensions;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Graph.DotnetCore.Core.Test.Extensions
+{
+    [TestClass]
+    public class ContentConverterExtensionsTests
+    {
+        [TestMethod]
+        public void ToDateTime_Should_Convert_DateTimeTimeZone_To_DateTime()
+        {
+            DateTimeTimeZone dateTimeTimeZone = new DateTimeTimeZone
+            {
+                TimeZone = "UTC",
+                DateTime = "2019-01-25T06:37:39.8058788Z"
+            };
+
+            var expectedDateTime = DateTime.Parse(dateTimeTimeZone.DateTime);
+            Assert.AreEqual(expectedDateTime, dateTimeTimeZone.ToDateTime());
+        }
+
+        [TestMethod]
+        public void ToDateTimeOffset_Should_Convert_DateTimeTimeZone_To_DateTimeOffset()
+        {
+            DateTimeTimeZone dateTimeTimeZone = new DateTimeTimeZone
+            {
+                TimeZone = "UTC",
+                DateTime = "2019-01-25T06:37:39.8058788Z"
+            };
+
+            var expectedDateTimeOffset = DateTimeOffset.Parse(dateTimeTimeZone.DateTime).UtcDateTime;
+            Assert.AreEqual(expectedDateTimeOffset, dateTimeTimeZone.ToDateTimeOffset());
+        }
+    }
+}
