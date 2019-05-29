58681ef81a75e6868647ff699d538749387c6b97
Titus Gicheru <v-tigich@microsoft.com>
2019-05-29 15:31:40 +0300

    Change the implementation methods to use DateTime.ParseExact()

---
+++ src/Microsoft.Graph/Models/Extensions/DateTimeZoneExtensions.cs
@@ -0,0 +1,68 @@
+﻿// ------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------
+
+using System;
+
+namespace Microsoft.Graph.Extensions
+{
+    /// <summary>
+    /// Implements Content Converter Extensions
+    /// </summary>
+    public static class DateTimeZoneExtensions
+    {
+        /// <summary>
+        /// Converts DateTimeTimeZone which is a Complex Type to DateTime
+        /// </summary>
+        /// <param name="dateTimeTimeZone"></param>
+        /// <param name="dateFormat"></param>
+        /// <param name="provider">provide CultureInfo.InvariantCulture as the default</param>
+        /// <returns></returns>
+        public static DateTime ToDateTime(this DateTimeTimeZone dateTimeTimeZone, string dateFormat, IFormatProvider provider)
+        {
+            DateTime dateTime = DateTime.ParseExact(dateTimeTimeZone.DateTime, dateFormat, provider);
+            return dateTime;
+        }
+
+        /// <summary>
+        /// Converts DateTimeTimeZone which is a Complex Type to DateTimeOffset
+        /// </summary>
+        /// <param name="dateTimeTimeZone"></param>
+        /// <param name="dateFormat"></param>
+        /// <param name="provider">provide CultureInfo.InvariantCulture as the default</param>
+        /// <returns></returns>
+        public static DateTimeOffset ToDateTimeOffset(this DateTimeTimeZone dateTimeTimeZone, string dateFormat, IFormatProvider provider)
+        {
+            DateTime dateTime = DateTimeOffset.ParseExact(dateTimeTimeZone.DateTime, dateFormat, provider).UtcDateTime;
+            return dateTime;
+        }
+
+        /// <summary>
+        /// Converts a Datetime parameter to its equivalent DateTimeTimeZone Complex Type
+        /// </summary>
+        /// <param name="dateTimeTimeZone"></param>
+        /// <param name="dateTime"></param>
+        /// <param name="dateFormat"></param>
+        /// <param name="provider">provide CultureInfo.InvariantCulture as the default</param>
+        /// <returns></returns>
+        public static DateTimeTimeZone FromDateTime(this DateTimeTimeZone dateTimeTimeZone, DateTime dateTime, string dateFormat, IFormatProvider provider)
+        {
+            dateTimeTimeZone.DateTime = dateTime.ToUniversalTime().ToString(dateFormat, provider);
+            return dateTimeTimeZone;
+        }
+
+        /// <summary>
+        /// Converts a DatetimeOffset parameter to its equivalent DateTimeTimeZone Complex Type
+        /// </summary>
+        /// <param name="dateTimeTimeZone"></param>
+        /// <param name="dateTimeOffset"></param>
+        /// <param name="dateFormat"></param>
+        /// <param name="provider">provide CultureInfo.InvariantCulture as the default</param>
+        /// <returns></returns>
+        public static DateTimeTimeZone FromDateTimeOffset(this DateTimeTimeZone dateTimeTimeZone, DateTimeOffset dateTimeOffset, string dateFormat, IFormatProvider provider)
+        {
+            dateTimeTimeZone.DateTime = dateTimeOffset.ToUniversalTime().ToString(dateFormat, provider);
+            return dateTimeTimeZone;
+        }
+    }
+}
---
+++ tests/Microsoft.Graph.DotnetCore.Test/Models/Extensions/DateTimeZoneExtensionsTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Graph.Extensions;
+using System;
+using Xunit;
+using System.Globalization;
+
+namespace Microsoft.Graph.DotnetCore.Test.Extensions
+{
+    public class DateTimeZoneExtensionsTests
+    {
+        [Fact]
+        public void ToDateTime_Should_Convert_DateTimeTimeZone_To_DateTime()
+        {
+            DateTimeTimeZone dateTimeTimeZone = new DateTimeTimeZone
+            {
+                TimeZone = "UTC",
+                DateTime = "2019-01-25T06:37:39.8058788Z"
+            };
+
+            var expectedDateTime = DateTime.ParseExact(dateTimeTimeZone.DateTime, @"yyyy-MM-ddTHH:mm:ss.fffffffK", CultureInfo.InvariantCulture);
+            Assert.Equal(expectedDateTime, dateTimeTimeZone.ToDateTime(@"yyyy-MM-ddTHH:mm:ss.fffffffK", CultureInfo.InvariantCulture));
+        }
+
+        [Fact]
+        public void ToDateTimeOffset_Should_Convert_DateTimeTimeZone_To_DateTimeOffset()
+        {
+            DateTimeTimeZone dateTimeTimeZone = new DateTimeTimeZone
+            {
+                TimeZone = "UTC",
+                DateTime = "2019-01-25T06:37:39.8058788Z"
+            };
+
+            var expectedDateTimeOffset = DateTimeOffset.ParseExact(dateTimeTimeZone.DateTime, "yyyy-MM-ddTHH:mm:ss.fffffffK", CultureInfo.InvariantCulture).UtcDateTime;
+            Assert.Equal(expectedDateTimeOffset, dateTimeTimeZone.ToDateTimeOffset("yyyy-MM-ddTHH:mm:ss.fffffffK", CultureInfo.InvariantCulture));
+        }
+
+        [Fact]
+        public void FromDateTime_Should_Convert_DateTime_To_DateTimeTimeZone()
+        {
+            DateTimeTimeZone dateTimeTimeZone = new DateTimeTimeZone
+            {
+                TimeZone = "",
+                DateTime = ""
+            };
+
+            var expectedDateTime = DateTime.ParseExact("2019-01-25T06:37:39.8058788Z", "yyyy-MM-ddTHH:mm:ss.fffffffK", CultureInfo.InvariantCulture).ToUniversalTime();
+            var actualDateTime = dateTimeTimeZone.FromDateTime(expectedDateTime, "yyyy-MM-ddTHH:mm:ss.fffffffK", CultureInfo.InvariantCulture);
+
+            Assert.Equal(expectedDateTime.ToString("yyyy-MM-ddTHH:mm:ss.fffffffK", CultureInfo.InvariantCulture), actualDateTime.DateTime);
+        }
+
+        [Fact]
+        public void FromDateTimeOffset_Should_Convert_DateTimeOffset_To_DateTimeTimeZone()
+        {
+            DateTimeTimeZone dateTimeTimeZone = new DateTimeTimeZone
+            {
+                TimeZone = "",
+                DateTime = ""
+            };
+
+            var expectedDateTimeOffset = DateTimeOffset.ParseExact("2019-01-25T06:37:39.8058788Z", "yyyy-MM-ddTHH:mm:ss.fffffffK", CultureInfo.InvariantCulture).ToUniversalTime();
+            var actualDateTimeOffset = dateTimeTimeZone.FromDateTimeOffset(expectedDateTimeOffset, "yyyy-MM-ddTHH:mm:ss.fffffffK", CultureInfo.InvariantCulture);
+
+            Assert.Equal(expectedDateTimeOffset.ToString("yyyy-MM-ddTHH:mm:ss.fffffffK", CultureInfo.InvariantCulture), actualDateTimeOffset.DateTime);
+        }
+    }
+}
