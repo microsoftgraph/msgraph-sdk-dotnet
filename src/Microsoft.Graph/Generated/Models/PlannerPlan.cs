8980c7be618e64f6f7e03bf1b0aca5b2bde4db61
Microsoft Graph DevX Tooling <GraphTooling@service.microsoft.com>
2022-08-17 09:55:43 +0000

    Update generated files with build 82954

--- src/Microsoft.Graph/Generated/Models/PlannerPlan.cs
+++ src/Microsoft.Graph/Generated/Models/PlannerPlan.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 namespace Microsoft.Graph.Models {
-    /// <summary>Provides operations to manage the collection of agreementAcceptance entities.</summary>
+    /// <summary>Provides operations to manage the collection of agreement entities.</summary>
     public class PlannerPlan : Entity, IParsable {
         /// <summary>Collection of buckets in the plan. Read-only. Nullable.</summary>
         public List<PlannerBucket> Buckets {
