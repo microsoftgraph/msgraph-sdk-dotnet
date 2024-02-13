20daafef639791a8cce35459a1d38e321129ba0b
Microsoft Graph DevX Tooling <GraphTooling@service.microsoft.com>
2024-02-13 10:50:41 +0000

    Update generated files with build 135049

--- src/Microsoft.Graph/Generated/Models/IntuneBrand.cs
+++ src/Microsoft.Graph/Generated/Models/IntuneBrand.cs
@@ -201,7 +201,7 @@ namespace Microsoft.Graph.Models {
         }
 #endif
         /// <summary>
-        /// Instantiates a new intuneBrand and sets the default values.
+        /// Instantiates a new <see cref="IntuneBrand"/> and sets the default values.
         /// </summary>
         public IntuneBrand() {
             BackingStore = BackingStoreFactorySingleton.Instance.CreateBackingStore();
@@ -210,6 +210,7 @@ namespace Microsoft.Graph.Models {
         /// <summary>
         /// Creates a new instance of the appropriate class based on discriminator value
         /// </summary>
+        /// <returns>A <cref="IntuneBrand"></returns>
         /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
         public static IntuneBrand CreateFromDiscriminatorValue(IParseNode parseNode) {
             _ = parseNode ?? throw new ArgumentNullException(nameof(parseNode));
@@ -218,6 +219,7 @@ namespace Microsoft.Graph.Models {
         /// <summary>
         /// The deserialization information for the current model
         /// </summary>
+        /// <returns>A <cref="IDictionary<string, Action<IParseNode>>"></returns>
         public virtual IDictionary<string, Action<IParseNode>> GetFieldDeserializers() {
             return new Dictionary<string, Action<IParseNode>> {
                 {"contactITEmailAddress", n => { ContactITEmailAddress = n.GetStringValue(); } },
--- src/Microsoft.Graph/Generated/Models/WebApplication.cs
+++ src/Microsoft.Graph/Generated/Models/WebApplication.cs
@@ -99,7 +99,7 @@ namespace Microsoft.Graph.Models {
         }
 #endif
         /// <summary>
-        /// Instantiates a new webApplication and sets the default values.
+        /// Instantiates a new <see cref="WebApplication"/> and sets the default values.
         /// </summary>
         public WebApplication() {
             BackingStore = BackingStoreFactorySingleton.Instance.CreateBackingStore();
@@ -108,6 +108,7 @@ namespace Microsoft.Graph.Models {
         /// <summary>
         /// Creates a new instance of the appropriate class based on discriminator value
         /// </summary>
+        /// <returns>A <cref="WebApplication"></returns>
         /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
         public static WebApplication CreateFromDiscriminatorValue(IParseNode parseNode) {
             _ = parseNode ?? throw new ArgumentNullException(nameof(parseNode));
@@ -116,6 +117,7 @@ namespace Microsoft.Graph.Models {
         /// <summary>
         /// The deserialization information for the current model
         /// </summary>
+        /// <returns>A <cref="IDictionary<string, Action<IParseNode>>"></returns>
         public virtual IDictionary<string, Action<IParseNode>> GetFieldDeserializers() {
             return new Dictionary<string, Action<IParseNode>> {
                 {"homePageUrl", n => { HomePageUrl = n.GetStringValue(); } },
