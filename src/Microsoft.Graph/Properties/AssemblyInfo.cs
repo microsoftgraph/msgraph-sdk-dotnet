3863f6d703df40a4e4e926153bede7ffd06eee52
Michael Mainer <8527305+MIchaelMainer@users.noreply.github.com>
2025-08-14 13:26:33 -0700

    chore: add copyright to assemblyinfo.cs
    This should trigger rp

--- src/Microsoft.Graph/Properties/AssemblyInfo.cs
+++ src/Microsoft.Graph/Properties/AssemblyInfo.cs
@@ -1,4 +1,8 @@
-﻿using System.Runtime.CompilerServices;
+﻿// ------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------
+
+using System.Runtime.CompilerServices;
 
 #if DEBUG
 [assembly: InternalsVisibleTo("Microsoft.Graph.DotnetCore.Test")]
